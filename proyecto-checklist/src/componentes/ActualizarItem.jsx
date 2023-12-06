2bc1988c3db3b2192ac42ea0f948f967a0ba3125
Julio Izquierdo Lopez <izquierdolopezjulhe.96@gmail.com>
2023-12-05 21:00:44 -0300

    Tercer Commit, solucion de errores

--- proyecto-checklist/src/componentes/ActualizarItem.jsx
+++ proyecto-checklist/src/componentes/ActualizarItem.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import { MdModeEdit } from "react-icons/md";
 
 export const ActualizarItem = ({nota, handleUpdateNota}) => {
+
+  const [disabled, setDisabled] = useState(true)
+
+  const focusInputRef = useRef();
+
+
+  const onFormSubmit = e => {
+    e.preventDefault()
+
+    setDisabled(!disabled)
+
+    focusInputRef.current.focus()
+  };
+
+
   return (
-    <form>
+    <form onSubmit={onFormSubmit}>
         <input 
         type="text" 
-        className='input-update '  
+        className={`input-update ${
+          nota.done ? 'text-decoration-dashed' : ''
+      }`}  
         name='description' 
         value={nota.description} 
         placeholder='¿Que Tarea quieres agregar?'/>
+        readOnly={disabled}
+        ref={focusInputRef}
 
         <button className="btn-edit" type='submit'><MdModeEdit /></button>
     </form>
